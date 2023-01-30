24f40345b670ca2574242e68c100a781e7b60c3f
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-01-30 13:54:52 +0300

    task 11

---
+++ src/fetchImages.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const fetchImages = async (inputValue, pageNr) => {
+  const response = await axios(
+    `https://pixabay.com/api/?key=33208441-52fb7929803924197e6b021fa&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`)
+ 
+  return response.data;   
+
+};
+   
--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-import './css/styles.css';
+import { fetchImages } from './fetchImages';
+import { markupImageList, gallery} from './markupImageList';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
+const inputSerch = document.querySelector('.search-form-input');
+const btnSearch = document.querySelector('.search-form-button');
+const btnLoadMore = document.querySelector('.load-more');
+
+let gallerySimpleLightbox = new SimpleLightbox('.gallery');
+let pageNumber = 1;
+btnLoadMore.style.display = 'none';
+
+
+btnSearch.addEventListener('click', async (e) => {
+  e.preventDefault();
+  initialValue();
+  const inputValue = inputSerch.value.trim();
+
+  if (inputValue !== '') {
+    
+    const foundData = await fetchImages(inputValue, pageNumber);
+   
+      if (foundData.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        markupImageList(foundData.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${foundData.totalHits} images.`
+        );
+        btnLoadMore.style.display = 'block';
+        gallerySimpleLightbox.refresh();
+      };
+  };
+});
+
+
+
+btnLoadMore.addEventListener('click', async() => {
+  pageNumber++;
+  const inputValue = inputSerch.value.trim();
+  btnLoadMore.style.display = 'none';
+  const foundData = await fetchImages(inputValue, pageNumber);
+ 
+    if (foundData.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      markupImageList(foundData.hits);
+      Notiflix.Notify.success(
+        `Hooray! We found ${foundData.totalHits} images.`
+      );
+      btnLoadMore.style.display = 'block';
+    };
+});
+
+
+
+function initialValue() {
+  gallery.innerHTML = '';
+  pageNumber = 1;
+  btnLoadMore.style.display = 'none';
+};
